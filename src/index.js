fd60d3839eb39542e4bc8c56a3a33aa41f25247a
Tycho Bokdam <tycho@palmtreecoding.com>
2019-10-18 11:25:57 +0200

    fix: Test release

--- src/index.js
+++ src/index.js
@@ -23,10 +23,14 @@ async function run() {
       } else {
         core.info(`Recommended release type: ${recommendation.releaseType}`)
 
+        // Bump the version in the package.json
         const jsonPackage = packageJson.bump(
           packageJson.get(),
           recommendation.releaseType,
         )
+
+        // Update the package.json file
+        packageJson.update(jsonPackage)
 
         core.info(`New version: ${jsonPackage.version}`)
 
@@ -43,14 +47,13 @@ async function run() {
 
         changelogStream
           .pipe(fs.createWriteStream('CHANGELOG.md'))
-          .on('finish',async () => {
+          .on('finish', async() => {
+            core.info('Push all changes')
             // Add changed files to git
             await git.add('.')
             await git.commit(commitMessage.replace('{version}', jsonPackage.version))
             await git.push()
-
           })
-
       }
     })
 
