8283447152aeee1a79d4f75b5f40319488f645ef
mshanemc <shane.mclaughlin@salesforce.com>
2022-10-12 08:34:20 -0500

    feat: support semver prerelease versioning

--- src/helpers/bumpVersion.js
+++ src/helpers/bumpVersion.js
@@ -1,7 +1,7 @@
-const core = require('@actions/core')
-const semverValid = require('semver').valid
+const core = require("@actions/core");
+const semverValid = require("semver").valid;
 
-const requireScript = require('./requireScript')
+const requireScript = require("./requireScript");
 
 /**
  * Bumps the given version with the given release type
@@ -11,58 +11,68 @@ const requireScript = require('./requireScript')
  * @returns {string}
  */
 module.exports = async (releaseType, version) => {
-  let major, minor, patch
+  let major, minor, patch;
 
   if (version) {
-    [major, minor, patch] = version.split('.')
+    [major, minor, patch] = version.match(/\d*\.\d*\.\d/)?.[0].split(".");
 
     switch (releaseType) {
-      case 'major':
-        major = parseInt(major, 10) + 1
-        minor = 0
-        patch = 0
-        break
+      case "major":
+        major = parseInt(major, 10) + 1;
+        minor = 0;
+        patch = 0;
+        break;
 
-      case 'minor':
-        minor = parseInt(minor, 10) + 1
-        patch = 0
-        break
+      case "minor":
+        minor = parseInt(minor, 10) + 1;
+        patch = 0;
+        break;
 
       default:
-        patch = parseInt(patch, 10) + 1
+        patch = parseInt(patch, 10) + 1;
     }
   } else {
-    let version = semverValid(core.getInput('fallback-version'))
+    let version = semverValid(core.getInput("fallback-version"));
 
     if (version) {
-      [major, minor, patch] = version.split('.')
+      [major, minor, patch] = version.match(/\d*\.\d*\.\d/)?.[0].split(".");
     } else {
       // default
-      major = 0
-      minor = 1
-      patch = 0
+      major = 0;
+      minor = 1;
+      patch = 0;
     }
 
-    core.info(`The version could not be detected, using fallback version '${major}.${minor}.${patch}'.`)
+    core.info(
+      `The version could not be detected, using fallback version '${major}.${minor}.${patch}'.`
+    );
   }
 
-  const preChangelogGenerationFile = core.getInput('pre-changelog-generation')
+  const preChangelogGenerationFile = core.getInput("pre-changelog-generation");
 
-  let newVersion = `${major}.${minor}.${patch}`
+  let newVersion = version
+    .replace(/\d*\.\d*\.\d/, `${major}.${minor}.${patch}`)
+    .replace(/(?<=-.*\.)\d+/, 0);
 
   if (preChangelogGenerationFile) {
-    const preChangelogGenerationScript = requireScript(preChangelogGenerationFile)
+    const preChangelogGenerationScript = requireScript(
+      preChangelogGenerationFile
+    );
 
     // Double check if we want to update / do something with the version
-    if (preChangelogGenerationScript && preChangelogGenerationScript.preVersionGeneration) {
-      const modifiedVersion = await preChangelogGenerationScript.preVersionGeneration(newVersion)
+    if (
+      preChangelogGenerationScript &&
+      preChangelogGenerationScript.preVersionGeneration
+    ) {
+      const modifiedVersion =
+        await preChangelogGenerationScript.preVersionGeneration(newVersion);
 
       if (modifiedVersion) {
-        core.info(`Using modified version "${modifiedVersion}"`)
-        newVersion = modifiedVersion
+        core.info(`Using modified version "${modifiedVersion}"`);
+        newVersion = modifiedVersion;
       }
     }
   }
 
-  return newVersion
-}
+  return newVersion;
+};
