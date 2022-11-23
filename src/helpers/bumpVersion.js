ecddc262291a3d768c04c52d31af23d1cf2e6d84
Patrick Dreker <patrick.dreker@codecamp-n.com>
2022-11-23 14:56:33 +0100

    fix: simplify logic fallback version logic, remove default from action.yaml

--- src/helpers/bumpVersion.js
+++ src/helpers/bumpVersion.js
@@ -19,11 +19,14 @@ module.exports = async (releaseType, version) => {
   if (version) {
     newVersion = semver.inc(version, (prerelease ? 'prerelease' : releaseType), identifier)
   } else {
-    let version = semver.valid(core.getInput('fallback-version'))
 
-    if (version) {
-      newVersion = version
-    } else {
+    const fallbackVersion = core.getInput('fallback-version')
+
+    if (fallbackVersion) {
+      newVersion = semver.valid(fallbackVersion)
+    }
+
+    if (!newVersion) {
       // default
       newVersion = (prerelease ? `0.1.0-${identifier}.0` : '0.1.0')
     }
