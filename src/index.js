91027ef2cddd2c6f60da35d5e9c5d74f5f5f17e7
tardis <14971857@qq.com>
2025-05-08 01:16:25 +0800

    feat: test message
    BETA_CHANGE
    RELEASE_CANDIDATE_CHANGE

--- src/index.js
+++ src/index.js
@@ -110,6 +110,7 @@ async function run() {
     })
 
     core.info(`Recommended release type: ${recommendation.releaseType}`)
+    core.info(`Recommended: ${recommendation}`)
 
     // If we have a reason also log it
     if (recommendation.reason) {
